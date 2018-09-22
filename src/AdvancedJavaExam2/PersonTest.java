4e11b0d69057116ed6ea649a03cca243137394c7
hanelso <gjsxj1992@gmail.com>
2018-09-23 00:40:15 +0900

    JACA EXAM -JAVA 시험 2번 문제 Main 수정

--- src/AdvancedJavaExam2/PersonTest.java
+++ src/AdvancedJavaExam2/PersonTest.java
@@ -2,14 +2,18 @@ package AdvancedJavaExam2;
 
 public class PersonTest {
 	public static void main(String[] args) {
-		Person person = new Person();
-		person = new Person();
 
-		Person person2 = new Person(19, "김철수");
-
-		person.selfIntroduce();
+		Person person1 = new Person(12,"김자바");
+		System.out.println("현재 인구수는 " + Person.getPopulation() + "명 입니다.");
+		Person person2 = new Person(27,"박자바");
+		System.out.println("현재 인구수는 " + Person.getPopulation() + "명 입니다.");
+		Person person3 = new Person(28, "이자바");
+		System.out.println("현재 인구수는 " + Person.getPopulation() + "명 입니다.");
+		
+		person1.selfIntroduce();
 		person2.selfIntroduce();
+		person3.selfIntroduce();
 
-		System.out.println(Person.getPopulation());
+		
 	}
 }
